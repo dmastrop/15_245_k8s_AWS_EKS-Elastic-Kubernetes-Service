2f8b46b29dfd11e1b35fba7f9ff7adea33168f76
Dave Mastropolo <davemastropolo@Margarets-MacBook-Air.local>
2023-05-19 15:24:59 -0700

    initial commit to master

---
+++ users-api/users-app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const userRoutes = require('./routes/user-routes');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+app.use(userRoutes);
+
+app.use((err, req, res, next) => {
+  let code = 500;
+  let message = 'Something went wrong.';
+  if (err.code) {
+    code = err.code;
+  }
+
+  if (err.message) {
+    message = err.message;
+  }
+  res.status(code).json({ message: message });
+});
+
+mongoose.connect(
+  process.env.MONGODB_CONNECTION_URI,
+  // this env variable is in the users.yaml file and is my atlas mongodb cluser uri
+  { useNewUrlParser: true },
+  (err) => {
+    if (err) {
+      console.log('COULD NOT CONNECT TO MONGODB!');
+    } else {
+      app.listen(3000);
+    }
+  }
+);
